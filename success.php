4a13fb030fea5a72d1b5b99388975054ea4379a7
antosyya <60662409+antosyya@users.noreply.github.com>
2020-02-11 18:13:18 +0300

    Update success.php

--- success.php
+++ success.php
@@ -7,7 +7,7 @@ $massage = htmlspecialchars($_POST["massage"]);
 $refferer = getenv('HTTP_REFERER');
 $date=date("d.m.y"); // число.месяц.год  
 $time=date("H:i"); // часы:минуты:секунды 
-$myemail = "tosyya@list.ru";
+$myemail = "tossya26@gmail.com";
 
 $tema = "Тема письма админу";
 $message_to_myemail = "Текст письма:
@@ -18,15 +18,15 @@ E-mail: $email<br>
 Источник (ссылка): $refferer
 ";
 
-mail($myemail, $tema, $message_to_myemail, "From: antosyya <60662409+antosyya@users.noreply.github.com> \r\n Reply-To: antosyya\r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
+mail($myemail, $tema, $message_to_myemail, "From: antosyya <tosyya@list.ru> \r\n Reply-To: antosyya\r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
 
-//$f = fopen("leads.xls", "a+");
-//fwrite($f," <tr>");    
-//fwrite($f," <td>$email</td> <td>$name</td> <td>$tel</td>   <td>$date / $time</td>");   
-//fwrite($f," <td>$refferer</td>");    
-//fwrite($f," </tr>");  
-//fwrite($f,"\n ");    
-//fclose($f);
+$f = fopen("leads.xls", "a+");
+fwrite($f," <tr>");    
+fwrite($f," <td>$email</td> <td>$name</td> <td>$massage</td>   <td>$date / $time</td>");   
+fwrite($f," <td>$refferer</td>");    
+fwrite($f," </tr>");  
+fwrite($f,"\n ");    
+fclose($f);
 
 
 ?>
