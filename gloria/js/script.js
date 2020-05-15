31fc4670c25a3880ac25f880b3d3cbf23b03cb34
antosyya <60662409+antosyya@users.noreply.github.com>
2020-05-15 22:01:33 +0300

    Add files via upload

---
+++ gloria/js/script.js
@@ -0,0 +1,11 @@
+
+    $('.slider').slick(
+        {
+        
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            prevArrow: null,
+            nextArrow: null,
+          }
+    );
